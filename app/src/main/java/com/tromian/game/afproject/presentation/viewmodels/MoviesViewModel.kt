b2c20eb984b49c79d57636730608c244ebf9af86
tromian <m22ttt@gmail.com>
2021-06-17 18:39:56 +0300

    Fixed reinitializing MoviesViewModel in main fragment. And added saving list to DB

--- app/src/main/java/com/tromian/game/afproject/presentation/viewmodels/MoviesViewModel.kt
+++ app/src/main/java/com/tromian/game/afproject/presentation/viewmodels/MoviesViewModel.kt
@@ -1,6 +1,5 @@
 package com.tromian.game.afproject.presentation.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
