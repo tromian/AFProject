891ff64150e425944707e64b11f0e91a89071715
tromian <m22ttt@gmail.com>
2021-09-27 19:15:59 +0300

    gradle file cleaned from clear dependence

--- app/src/main/java/com/tromian/game/afproject/AppConstants.kt
+++ app/src/main/java/com/tromian/game/afproject/AppConstants.kt
@@ -4,7 +4,6 @@ class AppConstants {
     companion object {
         var TMDB_API_KEY = BuildConfig.TMDB_API_KEY
         val BASE_URL_TMDB = "https://api.themoviedb.org/3/"
-        val LANGUAGE = "ru"
 
         val IMAGES_BASE_URL = "https://image.tmdb.org/t/p/"
         val POSTER_SIZE = "/w500"
@@ -13,7 +12,6 @@ class AppConstants {
         val ADULT_CONTENT_AGE = 16
         val NOT_ADULT_CONTENT_AGE = 13
 
-        val LOG = "MyLog"
         val NETWORK_PAGE_SIZE = 5
     }
 
